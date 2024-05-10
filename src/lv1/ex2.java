42d4491f799c7c738d2855a4d52826b09b60adc6
RyuSangBae <fbtkdqo0406@gmail.com>
2024-05-10 22:09:58 +0900

    ex2 commit

--- src/lv1/ex2.java
+++ src/lv1/ex2.java
@@ -5,8 +5,6 @@ public class ex2 {
     //프로그래머스 [PCCP 기출문제] 붕대 감기
 
     public static int solution(int[] bandage, int health, int[][] attacks) {
-        int answer = 0;
-
         int t = bandage[0]; //시전시간
         int x = bandage[1]; //초당 회복량
         int y = bandage[2]; //추가 회복량
@@ -15,55 +13,60 @@ public class ex2 {
 
         int user = health;
 
-        for(int i = 0; i < attacks[attacks.length-1][0]; i++) {
+        System.out.println("마지막 공격시간:"+attacks[attacks.length-1][0]);
+
+        for(int i = 1; i <= attacks[attacks.length-1][0]; i++) {
+
 
             ++bandageTime;
-            int attackTime = attacks[i][0];
-            int attackDamage = attacks[i][1];
+            System.out.print("시간 :"+ i);
+            System.out.print(" banTime :"+bandageTime);
+            System.out.print(" beforeUser :"+user);
 
-            bandageTime = attackTime;
+            for (int[] attack : attacks) {
+                int attackYN = attack[0];
+                int attackDamage = attack[1];
 
-            System.out.println("atTime :"+attackTime);
-            System.out.println("banTime :"+bandageTime);
-            System.out.println("atDmg :"+attackDamage);
-            System.out.println("beforeUser :"+user);
-
-
-            if( user < health ) {
-                user += (bandageTime*x);
-                System.out.println("heal!! : " + (bandageTime*x));
-
-                if(bandageTime/t > 0){
-                    user += (y*(bandageTime/t));
-
-                    System.out.println("plusHeal : "+(y*(bandageTime/t)));
-                }
-
-                if(user > 30) {
-                    user = 30;
+                if (attackYN == i) {
+                    bandageTime = 0;
+                    System.out.print(" attackTime :"+i);
+                    System.out.print(" attack! :" + attackDamage);
+                    user -= attackDamage;
                 }
             }
 
-            user -= attackDamage;
 
-            bandageTime = 0;
+            if( user < health && bandageTime > 0 ) {
+                user += x;
+                System.out.print(" heal!! : " +x);
 
-            System.out.println("afterUser :"+user);
+                if(bandageTime%t == 0 && bandageTime/t > 0){
+                    user += y;
 
-            if(health < 0) {
+                    System.out.print(" plusHeal : "+y);
+                }
+
+                if(user > health) {
+                    user = health;
+                }
+            }
+
+            System.out.println(" afterUser :"+user);
+
+            if(user <= 0) {
+                System.out.println("user 사망");
                 return -1;
             }
-            answer = user;
         }
 
 
-        return answer;
+        return user;
     }
 
     public static void main(String[] args) {
         int[] bandage = {5, 1, 5};
-        int health = 30;
-        int[][] attacks = {{2, 10}, {9, 15}, {10, 5}, {11, 5}};
+        int health = 45;
+        int[][] attacks = {{2, 10}, {9, 15}, {10, 5}, {11, 5}, {13, 19}};
 
         System.out.println(solution(bandage, health, attacks));
     }
