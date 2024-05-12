649b24fdb35e587d5753eb253b5417570d44e752
RyuSangBae <fbtkdqo0406@gmail.com>
2024-05-12 20:35:37 +0900

    ex5 commit

---
+++ src/lv1/ex5.java
@@ -0,0 +1,34 @@
+package lv1;
+
+public class ex5 {
+    //{PCCE} 이웃한 칸
+
+    public static int solution(String[][] board, int h, int w) {
+        int count = 0;
+
+        int n = board.length;
+
+        int[] dh = {0, 1, -1, 0};
+        int[] dw = {1, 0, 0, -1};
+
+        for(int i = 0; i <= 3; i++) {
+            int h_check = h + dh[i];
+            int w_check = w + dw[i];
+
+            if((h_check >= 0 && h_check < n) && (w_check >= 0 && w_check < n)) {
+              if(board[h][w].equals(board[h_check][w_check])) {
+                  count ++;
+              }
+            }
+        }
+        return count;
+    }
+
+
+    public static void main(String[] args) {
+        String[][] board = {{"blue", "red", "orange", "red"}, {"red", "red", "blue", "orange"}, {"blue", "orange", "red", "red"}, {"orange", "orange", "red", "blue"}};
+
+        System.out.println(solution(board, 1, 1));
+    }
+
+}
