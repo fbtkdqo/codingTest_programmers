5c3a6b0f0c54b6b456237ca1930938ff5713ba90
RYU <s_sfusy@naver.com>
2024-05-15 18:35:50 +0900

    ex7 commit

---
+++ src/lv1/ex8.java
@@ -0,0 +1,19 @@
+package lv1;
+
+import java.util.Arrays;
+
+public class ex8 {
+    //추억점수
+    public static int[] solution(String[] name, int[] yearning, String[][] photo) {
+        int[] answer = {};
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        String[] name = {};
+        int[] yearing = {};
+        String[][] photo = {};
+
+        System.out.println(Arrays.toString(solution(name, yearing, photo)));
+    }
+}
